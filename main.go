0374c8ed657c1287287c27072ecdde0c55717a87
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2020-07-22 08:39:02 +0200

    update main

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/bhakiyakalimuthu/myalgo/pkg/stringsalgo"
 )
 
 func main() {
-	// list1, _ := stringsalgo.Reader("/Users/bhakiyarajkalimuthu/Documents/db/vehicle_riding.csv", 0)
-	// list2, _ := stringsalgo.Reader("/Users/bhakiyarajkalimuthu/Documents/db/active_rides.csv", 2)
-	// diff := stringsalgo.Difference(list1, list2)
-	// log.Print(len(diff))
-	// log.Print(diff)
-
-	in := []string{"bbaa", "aaa", "cccccd", "dfdddy"}
-	op := stringsalgo.BabySorting(in, "y", nil)
-	fmt.Println(op)
+	list1, _ := stringsalgo.Reader("/Users/bhakiyarajkalimuthu/Documents/voi/rides/vehicle_riding.csv", 0)
+	list2, _ := stringsalgo.Reader("/Users/bhakiyarajkalimuthu/Documents/voi/rides/active_rides.csv", 2)
+	diff := stringsalgo.Difference(list1, list2)
+	log.Print(len(diff))
+	log.Print(diff)
+	// in := []string{"bbaa", "aaa", "cccccd", "dfdddy"}
+	// op := stringsalgo.BabySorting(in, "y", nil)
+	// fmt.Println(op)
 
 }

a0e0b4b5b148b39833ec39bb18d13009b7ce001e
Bhakiyaraj Kalimuthu <bhakiya.kalimuthu@gmail.com>
2020-07-28 11:30:28 +0200

    json builder

--- main.go
+++ main.go
@@ -1,19 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"strings"
 
 	"github.com/bhakiyakalimuthu/myalgo/pkg/stringsalgo"
 )
 
-func main() {
-	list1, _ := stringsalgo.Reader("/Users/bhakiyarajkalimuthu/Documents/voi/rides/vehicle_riding.csv", 0)
-	list2, _ := stringsalgo.Reader("/Users/bhakiyarajkalimuthu/Documents/voi/rides/active_rides.csv", 2)
-	diff := stringsalgo.Difference(list1, list2)
-	log.Print(len(diff))
-	log.Print(diff)
-	// in := []string{"bbaa", "aaa", "cccccd", "dfdddy"}
-	// op := stringsalgo.BabySorting(in, "y", nil)
-	// fmt.Println(op)
+// func main() {
+// 	list1, _ := stringsalgo.Reader("/Users/bhakiyarajkalimuthu/Documents/voi/rides/vehicle_riding.csv", 0)
+// 	list2, _ := stringsalgo.Reader("/Users/bhakiyarajkalimuthu/Documents/voi/rides/active_rides.csv", 2)
+// 	// list3, _ := stringsalgo.Reader("/Users/bhakiyarajkalimuthu/Downloads/fm.csv", 0)
+// 	diff := stringsalgo.Difference(list1, list2)
+// 	log.Print(len(diff))
+// 	log.Print(diff)
+// // in := []string{"bbaa", "aaa", "cccccd", "dfdddy"}
+// // op := stringsalgo.BabySorting(in, "y", nil)
+// // fmt.Println(op)
+// for _, i := range list3 {
+// 	trim(i)
+// }
 
+// log.Print(list3)
+// }
+
+func main() {
+	zone, _ := stringsalgo.JSONReader("/Users/bhakiyarajkalimuthu/Documents/voi/zones/zones.json")
+	op := make([]zones, 0, len(zone.Zones))
+	for _, z := range zone.Zones {
+		op = append(op, zones{
+			City: z.Name,
+			ID:   z.ZoneID,
+		})
+	}
+	println(len(zone.Zones))
+	output, _ := json.Marshal(&op)
+	log.Print(output)
+}
+func trim(str string) {
+	s1 := strings.Replace(str, ";", "", -1)
+	s2 := strings.Split(s1, "/")
+	println(s2[4])
+}
+
+// func builder(city string, id string) {
+// 	"zones":[{"city":"Berlin","id":"118"}]
+// }
+
+type zones struct {
+	City string `json:"city"`
+	ID   string `json:"id"`
 }
--- pkg/stringsalgo/finddifference.go
+++ pkg/stringsalgo/finddifference.go
@@ -2,7 +2,9 @@ package stringsalgo
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"os"
 )
 
@@ -37,4 +39,64 @@ func Reader(path string, col int) ([]string, error) {
 	}
 	return recordTOReturn, nil
 
+}
+
+func JSONReader(path string) (*zone, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, errors.New("failed to open file")
+	}
+	defer file.Close()
+	reader, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, errors.New("failed to read file data")
+	}
+	var z zone
+	if err := json.Unmarshal(reader, &z); err != nil {
+		return nil, errors.New("failed to unmarshal")
+	}
+	return &z, nil
+}
+
+type zone struct {
+	Zones []struct {
+		ZoneID              string      `json:"zone_id"`
+		Name                string      `json:"name"`
+		Country             string      `json:"country"`
+		CompanyName         string      `json:"company_name"`
+		CompanyID           string      `json:"company_id"`
+		Currency            string      `json:"currency"`
+		Vat                 float64     `json:"vat"`
+		StartCost           int         `json:"start_cost"`
+		MinuteCost          int         `json:"minute_cost"`
+		FeeOutOfZone        int         `json:"fee_out_of_zone"`
+		CreditsExchangeRate interface{} `json:"credits_exchange_rate"`
+		Language            string      `json:"language"`
+		DefaultLocale       string      `json:"default_locale"`
+		Locales             string      `json:"locales"`
+		TimeZone            string      `json:"time_zone"`
+		MaxSpeed            int         `json:"max_speed"`
+		Boundaries          struct {
+			Lo struct {
+				Lat float64 `json:"Lat"`
+				Lng float64 `json:"Lng"`
+			} `json:"Lo"`
+			Hi struct {
+				Lat float64 `json:"Lat"`
+				Lng float64 `json:"Lng"`
+			} `json:"Hi"`
+		} `json:"boundaries"`
+		BountyStateExpr  interface{} `json:"bounty_state_expr"`
+		BountyAmountExpr string      `json:"bounty_amount_expr"`
+		MinutePrice      []struct {
+			Day    string `json:"day"`
+			Period string `json:"period"`
+			Amount int    `json:"amount"`
+		} `json:"minute_price"`
+		SleepHours          interface{} `json:"sleep_hours"`
+		IsActive            bool        `json:"is_active"`
+		IsSleeping          bool        `json:"is_sleeping"`
+		ParkingMode         string      `json:"parking_mode"`
+		ParkingSnapshotMode string      `json:"parking_snapshot_mode"`
+	} `json:"zones"`
 }
